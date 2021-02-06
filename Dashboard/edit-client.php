c310b319b3022aa6e6ef6410aebb090d38e9a341
samnjor-alpha <njoroges740@gmail.com>
2021-02-06 20:02:15 +0300

    created nav and top bar separately

--- Dashboard/edit-client.php
+++ Dashboard/edit-client.php
@@ -30,22 +30,7 @@ $msg_class = "";
 <? include '../public/stylesheet.php'?>
 </head>
 <body class="sb-nav-fixed">
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="home.php">Tomai water supplies</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-    <form class="  form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-
-    </form>
-
-    <ul class="navbar-nav ml-sm-auto ml-md-auto ml-lg-auto ml-auto d-none d-sm-none d-md-inline-block d-lg-inline-block d-xl-inline-block ml-sm-0 ml-lg-0 ml-xl-0 ml-md-0 align-right">
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-
-                <a class="dropdown-item" href="process-data/logout.php">Logout</a>
-            </div>
-        </li>
-    </ul>
-</nav>
+<? include 'topbar.php'?>
 <div id="layoutSidenav">
     <div id="layoutSidenav_nav">
       <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
