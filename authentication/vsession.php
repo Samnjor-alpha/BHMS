f7394c5840de247abfdfd7659ca67338365fd13a
samnjor-alpha <njoroges740@gmail.com>
2021-02-19 11:41:33 +0300

    fixed 2auth bug
    Signed-off-by: samnjor-alpha <njoroges740@gmail.com>

--- authentication/vsession.php
+++ authentication/vsession.php
@@ -1,34 +1,31 @@
-
 <?php
 session_start();
-include_once '../authentication/config.php';
+include_once 'config.php';
 $_SESSION['msg'] = "";
 $_SESSION['msg_class'] = "";
 
 $atv = $_SESSION['id'];
-$sql_tv = "SELECT * FROM sessions WHERE admin_id='$atv' AND logged_in='1' AND created_at=CURRENT_DATE()";
+$sql_tv = "SELECT * FROM sessions WHERE admin_id='$atv' AND logged_in='0' AND created_at=CURRENT_DATE()";
 $result_atv = mysqli_query($conn, $sql_tv);
 
 
-if (mysqli_num_rows($result_atv) < 1){
+if (mysqli_num_rows($result_atv) < 1) {
 
     $_SESSION['msg'] = "Not authorised...";
     $_SESSION['msg_class'] = "alert-danger";
     header('location:../authentication');
-}else{
+} else {
 
     $atv_row = $result_atv->fetch_assoc();
 
-    $ssid=$atv_row['session_id'];
-    $as_id=$_SESSION['id'];
+    $ssid = $atv_row['session_id'];
+    $as_id = $_SESSION['id'];
     if (hash_equals($_SESSION['bhmstoken'], $atv_row['session_token'])) {
-
-
 
 
         if (!isset($_SESSION['bhmsemail'])) {
 
-            $del_stoken="DELETE  FROM sessions where admin_id='$as_id' and session_id = '$ssid'";
+            $del_stoken = "DELETE  FROM sessions where admin_id='$as_id' and session_id = '$ssid'";
 
             unset($_SESSION['bhmsemail']);
             unset($_SESSION['bhmsuser']);
@@ -38,7 +35,7 @@ if (mysqli_num_rows($result_atv) < 1){
                 $_SESSION['msg'] = "Login to continue...";
                 header('location:../authentication');
             }
-        }else {
+        } else {
             $time = $_SERVER['REQUEST_TIME'];
 
             /**
@@ -60,7 +57,7 @@ if (mysqli_num_rows($result_atv) < 1){
                 $_SESSION['msg'] = "Your session has expired...";
                 $_SESSION['msg_class'] = "alert-danger";
 
-                $del_stoken="DELETE  FROM sessions where admin_id='$as_id' and session_id = '$ssid'";
+                $del_stoken = "DELETE  FROM sessions where admin_id='$as_id' and session_id = '$ssid'";
 
                 if (mysqli_query($conn, $del_stoken)) {
                     $_SESSION['msg'] = "Your session has expired...";
@@ -75,18 +72,19 @@ if (mysqli_num_rows($result_atv) < 1){
          * is based on it and not the user's login time.
          */
         $_SESSION['LAST_ACTIVITY'] = $time;
-    }else{
+    } else {
 
         $_SESSION['msg'] = "token Verification failed";
         $_SESSION['msg_class'] = "alert-danger";
         unset($_SESSION['bhmsemail']);
         unset($_SESSION['bhmsuser']);
         unset($_SESSION['bhmstoken']);
-        $del_stoken="DELETE  FROM sessions where admin_id='$as_id' and session_id = '$ssid'";
+        $del_stoken = "DELETE  FROM sessions where admin_id='$as_id' and session_id = '$ssid'";
 
         if (mysqli_query($conn, $del_stoken)) {
 
             header('location:../authentication');
         }
 
-    }}
+    }
+}
