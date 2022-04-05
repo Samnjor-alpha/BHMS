bd88d261cfa39ac1ddbe1a34c0b363cb4e1f3e16
BoredApe <njoroges740@gmail.com>
2022-04-05 09:37:55 +0300

    removed verification

--- authentication/config.php
+++ authentication/config.php
@@ -1,3 +1,3 @@
 <?php
-//$conn = mysqli_connect("remotemysql.com", "boZoXSiaGl", "vzzU6fUq0j", "boZoXSiaGl");
-$conn = mysqli_connect("localhost", "root", "", "bhms");
+$conn = mysqli_connect("remotemysql.com", "boZoXSiaGl", "vzzU6fUq0j", "boZoXSiaGl");
+//$conn = mysqli_connect("localhost", "root", "", "bhms");
