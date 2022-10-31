4df2cdabf97d19fb4dbfd38c4752e6cbf4076b9d
Bored Ape <njoroges740@gmail.com>
2022-10-31 23:05:18 +0300

    update config

--- authentication/config.php
+++ authentication/config.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("remotemysql.com", "boZoXSiaGl", "vzzU6fUq0j", "boZoXSiaGl");
+$conn = mysqli_connect("sql.freedb.tech", "freedb_samnjor_admin", "p3hQJf@vf3Mhzrg", "freedb_bhms_sept");
 //$conn = mysqli_connect("localhost", "root", "", "bhms");
